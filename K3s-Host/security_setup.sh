26a46b7878f3c112bcc4d23760364ede0ec8c569
SangYeon Min <judemin2087@naver.com>
2025-04-09 16:01:17 +0900

    [UPDATE]: fail2ban 관련 설정 업데이트

--- K3s-Host/security_setup.sh
+++ K3s-Host/security_setup.sh
@@ -21,6 +21,10 @@ enabled = true
 maxretry = 10
 findtime = 600
 bantime = 86400
+# ssh 포트를 열어두어서 호스트 머신이
+# 차단하는 대상에게 sendmail하는 것에 너무 많은 리소스를 사용하는 것을 확인
+# 따라서 [sshd] jail에 대해 sshd는 차단하되 메일은 보내지 않도록 설정
+action = iptables[name=SSH, port=ssh, protocol=tcp]
 END
 
 sudo systemctl enable fail2ban --now
